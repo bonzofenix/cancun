9bf222207a973ba299910154d00fbcd7d01e95b2
Alan Moran <bonzofenix@gmail.com>
2014-09-18 18:09:31 +0000

    bump version

--- lib/cancun/highline.rb
+++ lib/cancun/highline.rb
@@ -7,7 +7,7 @@ class Cancun
     extend ::Forwardable
     include ::Highline::Color
 
-    def_delegators :hl, :ask, :say, :menu, :choose, :color
+    def_delegators :hl, :ask, :say, :menu, :choose, :color, :agree
 
     def hl
       @hl ||= HighLine.new
--- lib/cancun/version.rb
+++ lib/cancun/version.rb
@@ -1,3 +1,3 @@
 class Cancun
-  VERSION = "0.2.1"
+  VERSION = "0.2.2"
 end
